c269963b0871a72315b88a4e70775073c9b3e737
Avinash Dubey <speedofreigns619@gmail.com>
2025-09-19 22:12:08 +0530

    added few utils

--- src/components/MessagePopup/MessagePopup.jsx
+++ src/components/MessagePopup/MessagePopup.jsx
@@ -1,9 +1,10 @@
+import { random } from '../../utils';
 import './MessagePopup.css'
 
 const MessagePopup = () => {
 
     const random_msg = ['wassup foo, tired?', 'yo come on man, why stop?', 'you arent a quitter, are you?'];
-    const randomIndex = Math.floor(Math.random()*random_msg.length);
+    const randomIndex = random(0, random_msg.length - 1);
     const message = random_msg[randomIndex];
 
   return (
