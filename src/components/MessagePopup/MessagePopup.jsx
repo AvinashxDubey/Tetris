44adc477e42c1037d82ad8387a7b2c0f463b6b1c
Avinash Dubey <speedofreigns619@gmail.com>
2025-11-17 15:55:36 +0530

    added connection for message popup

--- src/components/MessagePopup/MessagePopup.jsx
+++ src/components/MessagePopup/MessagePopup.jsx
@@ -1,16 +1,29 @@
-import { random } from '../../utils';
 import './MessagePopup.css'
+import { useSelector } from 'react-redux'
 
-const MessagePopup = () => {
+const MessagePopup = (props) => {
 
-    const random_msg = ['wassup foo, tired?', 'yo come on man, why stop?', 'you arent a quitter, are you?'];
-    const randomIndex = random(0, random_msg.length - 1);
-    const message = random_msg[randomIndex];
+    // const random_msg = ['wassup foo, tired?', 'yo come on man, why stop?', 'you arent a quitter, are you?'];
+    // const randomIndex = random(0, random_msg.length - 1);
+    // const message = random_msg[randomIndex];
+
+    const isRunning = useSelector((state) => state.game.isRunning)
+    const gameOver = useSelector((state) => state.game.gameOver)
+
+    let message = ''
+    let isHidden = 'hidden'
+
+    if(gameOver) {
+      message = 'Game Over'
+      isHidden = ''
+    } else if(!isRunning){
+      message = 'Paused'
+      isHidden = ''
+    }
 
   return (
-    <div className='message-popup'>
-        <h1> Paused </h1>
-        <p>{message}</p>
+    <div className={`message-popup ${isHidden}`}>
+        <h1> {message} </h1>
     </div>
   )
 }
