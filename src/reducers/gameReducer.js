ab1683e1f68aaa6e83b2ad53aced50d1310f8623
Avinash Dubey <speedofreigns619@gmail.com>
2026-03-23 10:08:43 +0530

    move left and right

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,34 +1,52 @@
-import { GAME_OVER, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT, PAUSE, RESTART, RESUME, ROTATE } from "../actions"
+import {
+  GAME_OVER,
+  MOVE_DOWN,
+  MOVE_LEFT,
+  MOVE_RIGHT,
+  PAUSE,
+  RESTART,
+  RESUME,
+  ROTATE,
+} from "../actions";
 import { canMoveTo, defaultState, nextRotation } from "../utils";
 
 const gameReducer = (state = defaultState(), action) => {
-    const { shape, grid, x, y, rotation, nextShape, score, isRunning } = state
+  const { shape, grid, x, y, rotation, nextShape, score, isRunning } = state;
 
-    switch (action.type) {
-        case ROTATE: {
-            const newRotation = nextRotation(shape, rotation)
-            if (canMoveTo(shape, grid, x, y, newRotation)) {
-                return { ...state, rotation: newRotation }
-            }
-            return state;
-        }
-        case MOVE_LEFT:
-            return state;
-        case MOVE_RIGHT:
-            return state;
-        case MOVE_DOWN:
-            return state;
-        case RESUME:
-            return state;
-        case RESTART:
-            return state;
-        case PAUSE:
-            return state;
-        case GAME_OVER:
-            return state;
-        default:
-            return state;
+  switch (action.type) {
+    case ROTATE: {
+      const newRotation = nextRotation(shape, rotation);
+      if (canMoveTo(shape, grid, x, y, newRotation)) {
+        return { ...state, rotation: newRotation };
+      }
+      return state;
     }
-}
+    case MOVE_LEFT: {
+      // subtract 1 from the x and check if this new position is possible by calling `canMoveTo()
+      if (canMoveTo(shape, grid, x - 1, y, rotation)) {
+        return { ...state, x: x - 1 };
+      }
+      return state;
+    }
+    case MOVE_RIGHT: {
+      if (canMoveTo(shape, grid, x + 1, y, rotation)) {
+        return { ...state, x: x + 1 };
+      }
+      return state;
+    }
+    case MOVE_DOWN:
+      return state;
+    case RESUME:
+      return state;
+    case RESTART:
+      return state;
+    case PAUSE:
+      return state;
+    case GAME_OVER:
+      return state;
+    default:
+      return state;
+  }
+};
 
-export default gameReducer
+export default gameReducer;
